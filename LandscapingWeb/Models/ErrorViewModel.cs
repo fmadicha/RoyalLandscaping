287e622c727c4afcf4306342182e7f6e71cc2477
Farirai <fmadicha@gmail.com>
2022-05-10 12:07:09 +0200

    Add project files.

---
+++ LandscapingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LandscapingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
